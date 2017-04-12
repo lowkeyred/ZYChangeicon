7335295881c2ed880caf93c256a205c4efb27884
wxfhome <jheit@jheitdeiMac.local>
2017-04-12 09:15:24 +0800

    ChangeAPPIcon
    iOS 10.3 修改APPicon

---
+++ ZYChangeicon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYChangeicon
+//
+//  Created by jheit on 2017/4/12.
+//  Copyright © 2017年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZYChangeicon/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  changeicon
+//
+//  Created by jheit on 2017/4/11.
+//  Copyright © 2017年 lzy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)icon1:(id)sender {
+    [[UIApplication sharedApplication] setAlternateIconName:@"icon1" completionHandler:^(NSError * _Nullable error) {
+        
+    }];
+}
+- (IBAction)icon2:(id)sender {
+    [[UIApplication sharedApplication] setAlternateIconName:@"icon2" completionHandler:^(NSError * _Nullable error) {
+        
+    }];
+}
+- (IBAction)icon3:(id)sender {
+    [[UIApplication sharedApplication] setAlternateIconName:@"icon3" completionHandler:^(NSError * _Nullable error) {
+        
+    }];
+}
+- (IBAction)icon4:(id)sender {
+    [[UIApplication sharedApplication] setAlternateIconName:@"icon4" completionHandler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
